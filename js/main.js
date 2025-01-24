5e03a70ac4a092360e2df2f0be8c1e5b7e8b2a69
sekar004845 <119045284+sekar004845@users.noreply.github.com>
2025-01-24 01:24:36 -0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,3 +7,29 @@ $(window).on('scroll', function () {
 		$("#header-sticky").addClass("sticky-menu");
 	}
 });
+
+
+
+
+
+// Home Page News and Updates
+$(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items: 4, // Default number of items
+        nav: true, // Enable navigation arrows
+        navText: ["<i class='fas fa-chevron-left'></i>", "<i class='fas fa-chevron-right'></i>"], // FontAwesome icons for arrows
+        autoplay: true, // Enable autoplay
+        responsive: {
+            0: {
+                items: 1, // 1 item on mobile
+            },
+            600: {
+                items: 2, // 2 items on tablet
+            },
+            1000: {
+                items: 2, // 2 items on desktop
+            }
+        }
+    });
+});
+
